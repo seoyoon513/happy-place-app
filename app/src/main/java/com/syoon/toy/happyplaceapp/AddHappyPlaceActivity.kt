a46993f4cb60598a20e8c6db4d814152be60a546
seoyoon513 <110798031+seoyoon513@users.noreply.github.com>
2022-11-08 15:08:38 +0900

    feat: 화면 이동 추가
    - toolbar에 뒤로가기 기능 추가

--- app/src/main/java/com/syoon/toy/happyplaceapp/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/syoon/toy/happyplaceapp/AddHappyPlaceActivity.kt
@@ -1,11 +1,20 @@
 package com.syoon.toy.happyplaceapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
+        val tbrAddPlace = findViewById<Toolbar>(R.id.tbr_add_place)
+
+        setSupportActionBar(tbrAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // toolbar 뒤로가기
+        tbrAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/syoon/toy/happyplaceapp/MainActivity.kt
+++ app/src/main/java/com/syoon/toy/happyplaceapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.syoon.toy.happyplaceapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fabAddHappyPlace = findViewById<View>(R.id.fab_add_happy_place)
+        fabAddHappyPlace.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
 }
