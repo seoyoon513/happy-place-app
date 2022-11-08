206b5f336e9538b78458c4ea81d01a42ae9d2b45
seoyoon513 <110798031+seoyoon513@users.noreply.github.com>
2022-11-08 14:26:05 +0900

    ui: AddHappyPlaceActivity 추가
    - CustomNocationbar 테마 적용 & 세로모드 유지, 라벨 추가
    - toolbar 추가: CutomToolbar 테마 적용

---
+++ app/src/main/java/com/syoon/toy/happyplaceapp/AddHappyPlaceActivity.java
@@ -0,0 +1,14 @@
+package com.syoon.toy.happyplaceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddHappyPlaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_happy_place);
+    }
+}
