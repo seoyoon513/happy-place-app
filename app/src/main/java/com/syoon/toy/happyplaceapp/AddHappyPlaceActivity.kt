26fb9224674bb31cff32099e87aec3685cb55c67
syb8200 <syb8200@naver.com>
2022-11-09 22:03:18 +0900

    [FEAT] : DatePicker 구현

--- app/src/main/java/com/syoon/toy/happyplaceapp/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/syoon/toy/happyplaceapp/AddHappyPlaceActivity.kt
@@ -1,20 +1,57 @@
 package com.syoon.toy.happyplaceapp
 
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var cal = Calendar.getInstance()
+    private lateinit var dateSetListener: OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
 
         val tbrAddPlace = findViewById<Toolbar>(R.id.tbr_add_place)
+        var edtDate = findViewById<EditText>(R.id.edt_date)
 
         setSupportActionBar(tbrAddPlace)
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // toolbar 뒤로가기
         tbrAddPlace.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+        edtDate.setOnClickListener(this)
+
     }
+
+    override fun onClick(p0: View?) {
+        when(p0!!.id){
+            R.id.edt_date -> {
+                DatePickerDialog(this@AddHappyPlaceActivity, dateSetListener,
+                    cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        }
+    }
+
+    private fun updateDateInView(){
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault()) // 휴대폰 위치따라 현지 기본 날짜 포맷 사용
+        findViewById<EditText>(R.id.edt_date).setText(sdf.format(cal.time).toString()) // 캘린더 상의 시간 사용
+    }
+
 }
